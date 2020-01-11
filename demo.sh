0cf58594f4ca356acc9a7daf401279b4d026c634
edz <edz@edzdeMacBook-Air.local>
2020-01-11 18:14:59 +0800

    demo.sh

--- demo.sh
+++ demo.sh
@@ -1,4 +1,3 @@
-cd ..
 mkdir new
 cd new
 touch new.txt
@@ -7,3 +6,8 @@ cd ..
 mkdir newother
 cd newother
 mkdir sub
+cd ..
+cp new/new.txt newother/sub
+mv new/new.txt new/new1.txt
+mv newother/sub new2
+rm -r new*
