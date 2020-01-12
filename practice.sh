44efe3b57f6d3bd2bfd9a02c9ec403c362b08684
edz <edz@edzdeMacBook-Air.local>
2020-01-12 08:52:27 +0800

    practice

---
+++ practice.sh
@@ -0,0 +1,21 @@
+mkdir example
+mv ex01.c example
+cd example
+mv ex01.c example.c
+cd ..
+mkdir practice
+mv ex02.c practice
+cd practice
+mv ex02 practice.c
+cd ..
+rm -rf ex03.c
+touch README.md
+vi README.md
+## example/example.c
+
+判断是奇数还是偶数
+
+## practice/practice.c
+
+比较两个数大小
+
